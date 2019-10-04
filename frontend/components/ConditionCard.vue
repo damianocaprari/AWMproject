7b40e2893e351342342eadd6b7a2f6dcf0a45cba
matteopulega <45599916+matteopulega@users.noreply.github.com>
2019-10-04 18:02:50 +0200

    Add files via upload

---
+++ frontend/components/ConditionCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-card max-width="344" class="mx-auto condition-card">
+    <v-card-title>{{ condition.name }}</v-card-title>
+    <v-card-text>{{ condition.description }}</v-card-text>
+    <div class="action-buttons">
+        <nuxt-link :to="`/conditions/${condition.id}/`" class="btn btn-sm btn-success"> View </nuxt-link>
+        <nuxt-link :to="`/conditions/${condition.id}/edit/`" class="btn btn-sm btn-primary"> Edit </nuxt-link>
+        <button @click="onDelete(condition.id)"  class="btn btn-sm btn-danger">Delete</button>
+      </div>
+  </v-card>
+</template>
+
+<script>
+  export default {
+      props: ["condition", "onDelete"]
+  }
+</script>
+
+
+<style>
+  .condition-card {
+    box-shadow: 0 1rem 1.5rem rgba(0,0,0,.6);
+  }
+</style>
