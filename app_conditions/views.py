f2e0c1b6da4bc054e258c75676f41a3e5bf14ac7
mat <matteopulega@gmail.com>
2019-11-05 18:43:00 +0100

    TERMINATE permissions

--- app_conditions/views.py
+++ app_conditions/views.py
@@ -10,14 +10,9 @@ class ConditionViewSet(viewsets.ModelViewSet):
     queryset = Condition.objects.all()
     # permission_classes = (permissions.AllowAny,)
     permission_classes = (permissions.ReadOnly | permissions.IsAdminUser,)
-    # todo da cambiare con il livello appropriato
-    # TODO le permissions possono essere combinate usando & (and), | (or), ~ (not)
-    # ad esempio: permission_classes = (permissions.IsAuthenticated & IsOwnUser | permissions.IsAdminUser,)
 
 
 class ConditionCAViewSet(viewsets.ModelViewSet):
     queryset = ConditionCA.objects.all()
     serializer_class = ConditionCASerializer
-    permission_classes = (permissions.IsAdminUser,)  # todo da cambiare con il livello appropriato
-    # TODO le permissions possono essere combinate usando & (and), | (or), ~ (not)
-    # ad esempio: permission_classes = (permissions.IsAuthenticated & IsOwnUser | permissions.IsAdminUser,)
+    permission_classes = (permissions.IsAdminUser,)
